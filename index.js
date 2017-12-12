ee1996cc3e0fbc972201e7ec12ebfd5aaa3c362b
Maxime Gibeau <info@maxjbo.com>
2017-12-12 09:33:58 -0500

    🔧 Merged effects and themes into a single module

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const version = require('./package').version
 const BluetoothService = require('./lib/bluetooth-service')
 const StringsController = require('./lib/strings/controller')
-const effects = require('./lib/effects/effects')
-const themes = require('./lib/effects/themes')
+const effects = require('./lib/effects')
 
 module.exports = {
   version,
   BluetoothService,
   StringsController,
-  effects,
-  themes
+  effects
 }
